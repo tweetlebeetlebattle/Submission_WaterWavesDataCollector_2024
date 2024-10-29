bcd487ba5aeac1921752dc1379b1ec2703bc609a
tweetlebeetlebattle <boriskaravasilev3@gmail.com>
2024-10-29 07:49:08 +0200

    uploading

--- gatherAllData.py
+++
@@ -1,7 +0,0 @@
-from service.fetchNIMH_basic import *
-from service.fetchNIMH_gif import *
-from service.fetchStormGlass import *
-def gather():
-    fetchMeteoData()
-    fetchGifData()
-    fetchWeatherForAllLocations()
--- test.py
+++
@@ -1,2 +0,0 @@
-def testing():
-    return "interesting"
